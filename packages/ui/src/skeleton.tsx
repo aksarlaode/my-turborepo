bf495b0ba4d5cc4e1b2bb7111e1f69f59d3d66e1
aksarlaode <akhsaras21@gmail.com>
2024-05-22 07:16:37 +0000

    ui

---
+++ packages/ui/src/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@aksar/ui";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
